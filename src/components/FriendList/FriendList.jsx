99eb837ce184d9f4705e6b5f3cc9031aa7663efa
RenTensei <scs.bon7@gmail.com>
2023-04-29 18:04:42 +0300

    Implements components: FriendList, TransactionHistory

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(({ avatar, name, isOnline }) => (
+        <FriendListItem
+          key={name}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.array.isRequired,
+};
